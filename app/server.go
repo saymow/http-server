5b85febf65bdb71dd8e2a87354d9c8027b6d7d91
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-10-31 16:13:02 -0300

    feat: handle concurrent connections

--- app/server.go
+++ app/server.go
@@ -11,7 +11,7 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-type AppRequest struct {
+type HTTPProtocol struct {
 	path    string
 	headers map[string]string
 	body    string
@@ -25,45 +25,63 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-
-	appRequest := AppRequest{headers: make(map[string]string)}
-	parseRequest(&appRequest, buffer)
-
-	if appRequest.path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.HasPrefix(appRequest.path, "/echo") {
-		echoHandler(conn, &appRequest)
-	} else if strings.HasPrefix(appRequest.path, "/user-agent") {
-		userAgentHandler(conn, &appRequest)
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		go requestHanlder(conn)
 	}
 }
 
-func parseRequest(appRequest *AppRequest, requestBuffer []byte) {
+func requestHanlder(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+
+	protocol := HTTPProtocol{
+		headers: make(map[string]string),
+	}
+	parseProtocol(&protocol, buffer)
+
+	if protocol.path == "/" {
+		indexHandler(conn, &protocol)
+	} else if strings.HasPrefix(protocol.path, "/echo") {
+		echoHandler(conn, &protocol)
+	} else if strings.HasPrefix(protocol.path, "/user-agent") {
+		userAgentHandler(conn, &protocol)
+	} else {
+		notFoundHandler(conn, &protocol)
+	}
+}
+
+func parseProtocol(protocol *HTTPProtocol, requestBuffer []byte) {
 	requestStr := string(requestBuffer)
 	parts := strings.Split(requestStr, "\r\n")
 	target := strings.Split(parts[0], " ")
 	idx := 1
 
-	appRequest.path = target[1]
+	protocol.path = target[1]
 	for ; parts[idx] != ""; idx++ {
 		header := strings.Split(parts[idx], ": ")
-		appRequest.headers[header[0]] = header[1]
+		protocol.headers[header[0]] = header[1]
 	}
-	appRequest.body = parts[idx+1]
+
+	if idx+1 < len(parts) {
+		protocol.body = parts[idx+1]
+	} else {
+		protocol.body = ""
+	}
 }
 
-func userAgentHandler(conn net.Conn, request *AppRequest) {
+func indexHandler(conn net.Conn, request *HTTPProtocol) {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	conn.Close()
+}
+
+func userAgentHandler(conn net.Conn, request *HTTPProtocol) {
 	message := request.headers["User-Agent"]
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
@@ -73,12 +91,17 @@ func userAgentHandler(conn net.Conn, request *AppRequest) {
 	conn.Close()
 }
 
-func echoHandler(conn net.Conn, request *AppRequest) {
+func echoHandler(conn net.Conn, request *HTTPProtocol) {
 	message := strings.Replace(request.path, "/echo/", "", 1)
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
 	conn.Write([]byte("Content-Type: text/plain\r\n"))
 	conn.Write([]byte(fmt.Sprintf("Content-Length: %d\r\n\r\n", len(message))))
 	conn.Write([]byte(message))
+	conn.Close()
+}
+
+func notFoundHandler(conn net.Conn, request *HTTPProtocol) {
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	conn.Close()
 }
