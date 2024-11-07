c8c186f912c091d0309166299c2e7f3e118e6353
Gustavo Alves <gustavo_alves2010@yahoo.com.br>
2024-11-07 17:05:21 -0300

    feat: http response custom headers

--- app/server/server_integration_test.go
+++ app/server/server_integration_test.go
@@ -1,20 +1,30 @@
 package server
 
 import (
+	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"strings"
 	"testing"
 )
 
-func readResponse(conn net.Conn) (string, error) {
+type HTTPClientResponse struct {
+	Version        string
+	StatusCode     int
+	StatusCodeText string
+	Headers        map[string]string
+	Body           string
+}
+
+func readConnectionResponse(conn net.Conn) (string, error) {
 	var stringBuilder strings.Builder
 	buffer := make([]byte, 1024)
 
 	for {
 		n, err := conn.Read(buffer)
 
-		if err != nil {
+		if err != nil && err != io.EOF {
 			return stringBuilder.String(), err
 		}
 		if n == 0 {
@@ -25,6 +35,57 @@ func readResponse(conn net.Conn) (string, error) {
 	}
 
 	return stringBuilder.String(), nil
+}
+
+func readHTTPResponse(conn net.Conn) (*HTTPClientResponse, error) {
+	connResponse, err := readConnectionResponse(conn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	httpResponse := HTTPClientResponse{Headers: make(map[string]string)}
+
+	parts := strings.Split(connResponse, "\r\n")
+
+	if len(parts) == 0 {
+		return nil, fmt.Errorf("Invalid http response format")
+	}
+
+	target := strings.Split(parts[0], " ")
+
+	if len(target) != 3 {
+		return nil, fmt.Errorf("Invalid http response format")
+	}
+
+	statusCode, err := strconv.Atoi(target[1])
+
+	if err != nil {
+		return nil, fmt.Errorf("Invalid http response format")
+	}
+
+	// Read HTTP target
+	httpResponse.Version = target[0]
+	httpResponse.StatusCode = statusCode
+	httpResponse.StatusCodeText = target[2]
+
+	// Read HTTP headers
+	idx := 1
+	for ; idx < len(parts) && parts[idx] != ""; idx++ {
+		header := strings.Split(parts[idx], ": ")
+
+		if len(header) != 2 {
+			return nil, fmt.Errorf("Invalid http response format")
+		}
+
+		httpResponse.Headers[header[0]] = header[1]
+	}
+
+	if idx+1 < len(parts) {
+		httpResponse.Body = parts[idx+1]
+	}
+
+	return &httpResponse, nil
 }
 
 func TestBasicConnection(t *testing.T) {
@@ -48,11 +109,11 @@ func TestBasicConnection(t *testing.T) {
 		router.connectionHandler(server)
 	})()
 
-	response, _ := readResponse(client)
+	response, _ := readConnectionResponse(client)
 	Assert(t, strconv.Quote(response), strconv.Quote("HTTP/1.1 200 OK\r\n\r\n"))
 }
 
-func TestResponseBody(t *testing.T) {
+func TestResponseFormat(t *testing.T) {
 	client, server := net.Pipe()
 	router := Create()
 
@@ -74,7 +135,7 @@ func TestResponseBody(t *testing.T) {
 		router.connectionHandler(server)
 	})()
 
-	response, _ := readResponse(client)
+	response, _ := readConnectionResponse(client)
 	Assert(t, strconv.Quote(response), strconv.Quote("HTTP/1.1 200 OK\r\nContent-Type: plain/text\r\nContent-Length: 17\r\n\r\nthe response body"))
 }
 
@@ -101,5 +162,41 @@ func TestRouteParams(t *testing.T) {
 		router.connectionHandler(server)
 	})()
 
-	readResponse(client)
+	readConnectionResponse(client)
+}
+
+func TestCustomHeaders(t *testing.T) {
+	client, server := net.Pipe()
+	router := Create()
+
+	defer func() {
+		client.Close()
+		server.Close()
+	}()
+
+	router.Get("/", func(protocol *HTTPProtocol, response *HTTPResponse) {
+		response.SetHeader("Set-Cookie", "key=value; HttpOnly")
+		response.SetHeader("Cache-Control", "max-age=604800")
+		response.Body("a rather expensive body")
+		response.Send()
+	})
+
+	go (func() {
+		client.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
+	})()
+
+	go (func() {
+		router.connectionHandler(server)
+	})()
+
+	response, _ := readHTTPResponse(client)
+
+	Assert(t, response.Version, "HTTP/1.1")
+	Assert(t, response.StatusCode, 200)
+	Assert(t, response.StatusCodeText, "OK")
+	Assert(t, response.Headers["Content-Type"], "plain/text")
+	Assert(t, response.Headers["Content-Length"], "23")
+	Assert(t, response.Headers["Cache-Control"], "max-age=604800")
+	Assert(t, response.Headers["Set-Cookie"], "key=value; HttpOnly")
+	Assert(t, response.Body, "a rather expensive body")
 }
